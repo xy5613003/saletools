6888706b78f81151cf51222bc8b952f9d10cc9cd
feng <xy5613003@qq.com>
2024-04-25 15:46:33 +0800

    图片转换

---
+++ src/main/java/com/fight/ConvertPng.java
@@ -0,0 +1,65 @@
+package com.fight;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.concurrent.TimeUnit;
+
+@Log4j2
+public class ConvertPng {
+    public static void convertPngToJpg(String pngFilePath, String jpgFilePath) {
+
+        try {
+            if (!StringUtils.endsWithIgnoreCase(pngFilePath, ".png")) {
+                return;
+            }
+            // 1. 读取PNG图像文件
+            BufferedImage pngImage = ImageIO.read(new File(pngFilePath));
+
+            // 2. 创建新的BufferedImage对象
+            BufferedImage jpgImage = new BufferedImage(pngImage.getWidth(), pngImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+            // 3. 绘制PNG图像到新的BufferedImage对象
+            jpgImage.createGraphics().drawImage(pngImage, 0, 0, null);
+
+            // 4. 创建File对象用于保存JPG图像文件
+            File jpgFile = new File(jpgFilePath);
+
+            // 5. 将新的BufferedImage对象写入JPG图像文件
+            ImageIO.write(jpgImage, "jpg", jpgFile);
+        } catch (IOException e) {
+            log.error("文件不存在", e);
+        }
+    }
+
+
+    public static void watchFileStart(String dir) {
+        if (!Files.exists(Paths.get(dir))) {
+            return;
+        }
+        try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
+            Path path = Paths.get(dir);
+            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+            while (true) {
+                WatchKey key = watchService.take();
+                TimeUnit.SECONDS.sleep(2);
+                key.pollEvents().forEach(poll -> {
+                    Path subpath = (Path) poll.context();
+                    Path resolve = path.resolve(subpath);
+                    convertPngToJpg(resolve.toString(), path.resolve("temp").resolve(subpath.toString().replaceAll("(.png|.PNG)", ".jpg")).toString());
+                });
+                key.reset();
+            }
+
+        } catch (IOException e) {
+            log.error("watch error", e);
+        } catch (InterruptedException e) {
+            log.error("InterruptedException error", e);
+        }
+    }
+}
--- src/main/java/com/fight/Main.java
+++ src/main/java/com/fight/Main.java
@@ -1,8 +1,10 @@
 package com.fight;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 @Log4j2
@@ -11,18 +13,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         log.info("请选择功能:1->计算价格,2->选品");
         if (scanner.hasNext()) {
-            int select = scanner.nextInt();
+            String select = scanner.nextLine();
             switch (select) {
-                case 2 -> {
+                case "2" -> {
                     ClipboardListener clipboardListener = new ClipboardListener();
                     log.info("选品监听ing");
                 }
-                default -> {CalculateListener clipboardListener = new CalculateListener();
-                    log.info("价格计算监听ing");}
+                default -> {
+                    CalculateListener clipboardListener = new CalculateListener();
+                    log.info("请输入监控dir");
+                    String next = scanner.nextLine();
+                    CompletableFuture.runAsync(() -> ConvertPng.watchFileStart(StringUtils.isEmpty(next) ? "d:\\新建文件夹" : next));
+                    log.info("开始监控转换图片" + next);
+                }
 
             }
-
+            TimeUnit.SECONDS.sleep(150000);
         }
-        TimeUnit.SECONDS.sleep(150000);
     }
 }
